ed66b1b8abb563283c5a22d856a6ba819748d739
Siddhant Yadav <sidhantyadav1999@gmail.com>
2024-01-10 21:03:09 -0800

    added environment variables

--- server/index.js
+++ server/index.js
@@ -3,16 +3,21 @@ import cors from "cors"
 import mongoose from "mongoose"
 // import { ArtistRouter } from "./Router/ArtistRouter.js"
 import { SongRouter } from "./Router/SongRouter.js"
+import dotenv from "dotenv";
 
 const app = express()
 app.use(express.json())
+dotenv.config()
 app.use(cors())
 // app.use("/getArtists",ArtistRouter)
 app.use("/songs",SongRouter)
+const port = process.env.PORT || 3000
 
-mongoose.connect("DatabaseLink" );
+mongoose.connect(
+    process.env.MONGODB_URL  
+  );
   
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("Working Fine")
 })
